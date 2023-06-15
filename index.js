79ec977641805809aa7e32ae8423225f6602a937
Ajowi254 <ajowi.bery@gmail.com>
2023-06-15 14:44:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+ var customerName='bob';
+//function to uppercase
+  function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    };
+
+
+  function setBestCustomer() {
+    return bestCustomer = 'not bob';
+  }
+
+  function overwriteBestCustomer() {
+   return bestCustomer='maybe bob';
+    }
+
+    const leastFavoriteCustomer='Barnes';
+    function changeLeastFavoriteCustomer(){
+
+    }
+    function changeLeastFavoriteCustomer(){
+        return leastFavoriteCustomer='Barnes';
+    }
